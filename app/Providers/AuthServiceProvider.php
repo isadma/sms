fcd176f3b2354c89855040c492cba6a76bb1c504
Maksat Meredow <meredowm645@gmail.com>
2020-11-08 10:52:33 +0530

    =

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('viewWebSocketsDashboard', function ($user = null) {
+            return in_array($user->username, [
+                'admin'
+            ]);
+        });
     }
 }
